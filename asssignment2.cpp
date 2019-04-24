288843d435ab56ae742b655086a55d32517ac349
leederrick1 <36055294+leederrick1@users.noreply.github.com>
2019-04-23 22:35:37 -0700

    Add files via upload

---
+++ asssignment2.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+
+int main(){
+
+    int choice;
+
+  std:: cout << "Welcome to the Fishing and Scuba Shop\n" 
+       << "The shop sells these things: "
+       << "Nets, Fishing rods, hooks, scuba tank, wet suit, and flippers\n"
+       << "Fishings rods come with hooks, and the scuba tank comes with a wet suit\n"
+       << "Please enter a number to purchase \n"
+       << "1. Nets, 2. Fishing rods, 3. Hooks, 4. Scuba Tank, 5. Wet Suit, 6. Flippers\n";
+  std:: cin >> choice;
+  
+  switch(choice){
+
+      case 1: cout << ""
+      case 2: cout << ""
+      case 3: cout << ""
+      case 4: cout << ""
+      case 5: cout << ""
+      case 6: cout << ""
+      default: cout << ""
+  }
+    return 0;
+}
